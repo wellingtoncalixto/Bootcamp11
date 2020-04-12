eba1a5783f372c93416ec3680f1b8b3e47de2d31
wellington_calixto <wac.monti@outlook.com>
2020-04-11 21:00:21 -0300

    Aulas e Desafio de React do modulo 1 feitas

--- DesafiosModulo1/backend/src/index.js
+++ DesafiosModulo1/backend/src/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors');
 const {uuid, isUuid} = require('uuidv4')
 
 const api = express();
 
+api.use(cors())
 api.use(express.json());
 
 const repositories = [];
---
+++ modulo1/frontend/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css'
+// import { Container } from './styles';
+
+export default function App() {
+  return (
+    <h1>Hello word</h1>
+  );
+}
